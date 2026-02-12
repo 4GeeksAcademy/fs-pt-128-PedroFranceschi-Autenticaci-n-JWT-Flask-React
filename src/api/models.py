90b51596ba50eb8a7e24bae834262840c3f43566
Pedro Franceschi <117317386+FranceschiPedro@users.noreply.github.com>
2026-02-12 22:37:04 +0000

    Backend auth ready

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,29 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
+from flask_bcrypt import Bcrypt, generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    password_hash: Mapped[str] = mapped_column(nullable=False) # Por que no dejar solo password? PREGUNTA
 
+    #para hashear la contrasena --|| Agarra el pasword de nuestro mappeado arriba ^
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password).decode('utf-8')
+
+    #para comparar pasword encriptado Vs no encriptado
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    #Una vez hecho set/check_password.. ahora necesitamos crear un usuario y contrasena (Testing purposes)
+    # se realiza en routes.py BACKEND mediante una peticion. 
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
-        }
+        } 
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,8 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from sqlalchemy import select
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +22,53 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/register', methods=['POST'])
+def register_user():
+    #estamos recibiendo informacion del usuario desde el front end, por eso es DATA
+    data = request.get_json()
+    if not data.get("email") or not data.get("password"):
+        return jsonify({"Error": "Email and Password required"}), 400
+    
+    #checkeamos is el user existe, por email, ya que si no existe.. no tiene ID
+    existing_user = db.session.execute(select(User).where(User.email == data.get("email"))).scalar_one_or_none()
+    if existing_user: # si ya existe, no se puede crear de nuevo.
+        return jsonify({"Error": "User already exist."}), 400
+    
+    #si NO existe, entonces lo creamos  --|| new_user busca a mi modelo USER, y en nuestra nueva variable email, ponemos la data que nos arroja el fetch?
+    new_user = User(email = data.get("email"))
+    new_user.set_password(data.get("password")) #seteamos el password ORIGINAL a el user. con nuestro metodo del modelo USER. 
+    #( javier dice que esto solo GENERA el password)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"msje": "User created succesfully."}), 201
+
+# ahora toca hacer la ruta para login
+@api.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    if not data.get("email") or not data.get("password"):
+        return jsonify({"Error": "Email and Password required"}), 400
+    
+    user = db.session.execute(select(User).where(User.email == data.get("email"))).scalar_one_or_none()
+    
+    # check si user existe
+    if user is None:
+        return jsonify({"msg": "Inavlid username or password"}), 400
+    
+    if user.check_password(data.get("password")):
+        access_token = create_access_token(identity=str(user.id)) # despues de haber configurado mi .env y Flask-JWT-Extended en app.py
+        return jsonify({"msg": "Login successful", "token": access_token}), 200 # aqui agrego |"token": access_token|
+    else:
+        return jsonify({"msg": "Inavlid username or password"}), 400 
+    
+@api.route("/profile", methods=["GET"])
+@jwt_required()
+def get_profile():
+    user_id = get_jwt_identity() #se encarga de desencriptar la identidad del usuario |access_token|
+    user = db.session.get(User, int(user_id)) #proviene de modern method to get UNIQUE ID
+    if not user:
+        return jsonify({"msg": "User not found"}), 400
+    return jsonify(user.serialize()), 200
